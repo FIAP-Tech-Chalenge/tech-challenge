c83ae7f3227f51e9e83b7b05c3557b52b058eb8f
Lucas Rigon <lucas@bussolasocial.com.br>
2023-12-14 21:47:49 -0300

    Changes by ricar repository

--- src/main/java/com/fiap/tech/infra/repository/ClienteRepository.java
+++ src/main/java/com/fiap/tech/infra/repository/ClienteRepository.java
@@ -1,11 +1,14 @@
 package com.fiap.tech.infra.repository;
 
-import com.fiap.tech.domain.entity.cliente.Cliente;
 import com.fiap.tech.infra.model.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+
+    ClienteModel findByCpf(String cpf);
 
 }
