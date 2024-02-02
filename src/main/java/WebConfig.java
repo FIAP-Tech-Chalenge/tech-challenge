62d42c7fb6dc24beb32cf063c1764ad7954d3f7a
wolwerr <ricardo@dtmm.com.br>
2024-02-01 23:07:18 -0300

    "Adicionando WebConfig"

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://fiap-production.up.railway.app/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
