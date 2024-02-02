fd243826f5d3cab804d61746bd7840e60b8e9d4a
wolwerr <ricardo@dtmm.com.br>
2024-02-01 23:03:14 -0300

    "Adicionando WebConfig"

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -1,3 +1,4 @@
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://fiap-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
