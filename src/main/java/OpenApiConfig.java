9c41f983f579a38eab97694f8eda1f8351e4af72
wolwerr <ricardo@dtmm.com.br>
2024-02-01 23:30:06 -0300

    "Acertando Webconfig"

--- src/main/java/OpenApiConfig.java
+++ src/main/java/OpenApiConfig.java
@@ -10,6 +10,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().addServersItem(new Server().url("https://fiap-production.up.railway.app"))
-                .addServersItem(new Server().url("http://localhost:8080"));
+                .addServersItem(new Server().url("http://localhost:8080"))
+                .addServersItem(new Server().url("http://fiap-production.up.railway.app"));
     }
 }
