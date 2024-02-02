818a3244b387ec19727180185c288be89d66f316
wolwerr <ricardo@dtmm.com.br>
2024-02-01 23:23:47 -0300

    "Acertando Webconfig"

---
+++ src/main/java/OpenApiConfig.java
@@ -0,0 +1,17 @@
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("https://fiap-production.up.railway.app"))
+                .addServersItem(new Server().url("http://localhost:8080"));
+    }
+}
