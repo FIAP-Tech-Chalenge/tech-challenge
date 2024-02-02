c8d23a121d899a9f8159121685950642a72952eb
wolwerr <ricardo@dtmm.com.br>
2024-02-01 21:20:59 -0300

    "Alterando test prod"

--- src/test/java/com/fiap/tech/TechApplicationTests.java
+++ src/test/java/com/fiap/tech/TechApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TechApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
